7b55a4ab42a8e8f03a728e2f3480968f2dc72356
Kaiquy Silva <kaiquysilva63@gmail.com>
2023-06-06 12:34:45 -0300

    Inicio controle de versao

---
+++ src/main/java/com/itb/inf2fm/comercio/Produto.java
@@ -0,0 +1,22 @@
+package com.itb.inf2fm.comercio;
+
+public class Produto {
+	
+	private Long id;
+	private String nome;
+	private String descriçao;
+	private String codigoBarras;
+	private double preco;
+	
+	// public : Acesso liberado para todas as classes
+	
+	// private : Acesso permitido apenas para os membros
+    //            da propria classe.
+	//              Entende-se por membros(atributos e ou metodos)
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
