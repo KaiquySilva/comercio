924e1cbf246ed03245f5bc498250e965e3f3f3f8
Kaiquy Silva <kaiquysilva63@gmail.com>
2023-06-13 12:31:38 -0300

    Ajustes nos arquivos LojaController e Produto

--- src/main/java/com/itb/inf2fm/comercio/Produto.java
+++ src/main/java/com/itb/inf2fm/comercio/Produto.java
@@ -13,10 +13,49 @@ public class Produto {
 	// private : Acesso permitido apenas para os membros
     //            da propria classe.
 	//              Entende-se por membros(atributos e ou metodos)
-
-	public static void main(String[] args) {
-		
+	
+	// Metodos Setter ´s e Getter ´ s : atribuir e recuperar dados do atributo "RESPECTIVAMENTE"
+	
+	public void setId(Long id) {
+	     this.id = id;
+	}
+	public Long getId() {
+		return id;
+	}
+	
+	public void setNome(Long Nome, String nome) {
+		this.nome = nome;
 
 	}
-
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setDescriçao(String descriçao) {
+		this.descriçao = descriçao;
+	}
+	public String getDescriçao() {
+		return descriçao;
+    }
+	
+	public String setcodigoBarras() {
+		return codigoBarras;
+    }
+	
+	public String getcodigoBarras() {
+		return codigoBarras;
+	
+    }
+	public void setpreco(Long preco) {
+	     this.preco = preco;
+	
+	
+    }
+	
+	public double getPreco() {
+		return preco;
 }
+}
+
+
+
