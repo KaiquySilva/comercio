0750920664cbbf84833126f4090c969ce3e1e10c
Kaiquy Silva <kaiquysilva63@gmail.com>
2023-06-14 08:18:28 -0300

    Ediçao da LojaController

--- src/main/java/com/itb/inf2fm/comercio/Produto.java
+++ src/main/java/com/itb/inf2fm/comercio/Produto.java
@@ -4,9 +4,42 @@ public class Produto {
 	
 	private Long id;
 	private String nome;
-	private String descriçao;
+	private String descricao;
 	private String codigoBarras;
 	private double preco;
+	
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getCodigoBarras() {
+		return codigoBarras;
+	}
+	public void setCodigoBarras(String codigoBarras) {
+		this.codigoBarras = codigoBarras;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
 	
 	// public : Acesso liberado para todas as classes
 	
@@ -16,46 +49,8 @@ public class Produto {
 	
 	// Metodos Setter ´s e Getter ´ s : atribuir e recuperar dados do atributo "RESPECTIVAMENTE"
 	
-	public void setId(Long id) {
-	     this.id = id;
-	}
-	public Long getId() {
-		return id;
-	}
 	
-	public void setNome(Long Nome, String nome) {
-		this.nome = nome;
-
-	}
-	public String getNome() {
-		return nome;
-	}
-	
-	public void setDescriçao(String descriçao) {
-		this.descriçao = descriçao;
-	}
-	public String getDescriçao() {
-		return descriçao;
-    }
-	
-	public String setcodigoBarras() {
-		return codigoBarras;
-    }
-	
-	public String getcodigoBarras() {
-		return codigoBarras;
-	
-    }
-	public void setpreco(Long preco) {
-	     this.preco = preco;
-	
-	
-    }
-	
-	public double getPreco() {
-		return preco;
-}
-}
+  }
 
 
 
--- src/main/java/com/itb/inf2fm/comercio/controller/LojaController.java
+++ src/main/java/com/itb/inf2fm/comercio/controller/LojaController.java
@@ -1,18 +1,57 @@
 package com.itb.inf2fm.comercio.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.itb.inf2fm.comercio.Produto;
 
 @Controller
 @RequestMapping("/comercio/produtos")
 public class LojaController {
 	
+	List<Produto> ListaDeProdutos = new ArrayList<Produto>();
+	
 	@GetMapping("/listar")
 	public String listarProdutos() {
+		
+		Produto p1 = new Produto();
+		p1.setId(20l);
+		p1.setNome ( "Maquina de lavar Brastemp 15 1");
+		p1.setCodigoBarras("1455JIFJDUE212456");
+		p1.setDescricao("Produto Linha Branca Com painel digital");
+		p1.setPreco(3215.89);
+		
+
+		Produto p2 = new Produto();
+		p2.setId(20l);
+		p2.setNome("Televisor 70");
+		p2.setCodigoBarras("TLERD46589BVRD");
+		p2.setDescricao("Televisor Tela Plana let Samsung");
+		p2.setPreco(6326.12);
+		
+		Produto p3 = new Produto();
+		p3.setId(20l);
+		p3.setNome("Smartphone ");
+		p3.setCodigoBarras("SADCOK4590OMNB");
+		p3.setDescricao("Smartphone motorola g60");
+		p3.setPreco(1700.99);
+		
+		// Adicionando os produtos a lista
+		
+		 ListaDeProdutos.add(p1);
+		 
+		
+		
+		
+		
+		
 		
 		return "produtos";
 		
 
-}
+   }
 }
